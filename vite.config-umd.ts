64747410c34b569fd0fa315bb87663b4b961ff36
Sebastian <sebastian@stryczek.pl>
2025-01-06 16:00:28 +0100

    Improve dev experience

--- vite.config-umd.ts
+++ vite.config-umd.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   build: {
     outDir: "build",
     minify: isProduction,
-    sourcemap: !isProduction,
+    sourcemap: true,
     lib: {
       entry: resolve(__dirname, "src/markerlayout.ts"),
       name: "maptilermarkerlayout",
